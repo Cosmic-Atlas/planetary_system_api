dd2ad3ec1ec6a91edd2f534c9b36a1c63aa10f31
KaraJH <karas.jones@yahoo.com>
2023-09-29 18:39:16 -0600

    feat: Planet model with validations

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,3 +1,12 @@
 class Planet < ApplicationRecord 
-  
+  belongs_to :planetary_system
+  validates_presence_of :name, :planet_type, :year_discovered
+  validates :confirmed, inclusion: [true, false]
+  before_save :capitalize_planet_name
+
+  scope :only_true, -> {Planet.where(confirmed: true)}
+
+  def capitalize_planet_name 
+    self.name = name.capitalize
+  end
 end
