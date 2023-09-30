6e072cefbfbd06ad7fe553ca97dea2d74c7be36f
KaraJH <karas.jones@yahoo.com>
2023-09-30 15:02:27 -0600

    test: Add order tests

--- spec/models/planetary_system_spec.rb
+++ spec/models/planetary_system_spec.rb
@@ -15,4 +15,47 @@ RSpec.describe PlanetarySystem do
   describe 'relationship' do 
     it {should have_many :planets}
   end
+
+  describe '::order_by_created_at' do 
+    it 'orders the rows by when they were created at' do 
+      the_solar_system = PlanetarySystem.create!(name: "The Solar System", light_years_from_earth: 0, star_age: 4_600_000_000, created_at: Time.now)
+      tau_ceti_system = PlanetarySystem.create!(name: "Tau Ceti", light_years_from_earth: 12, star_age: 5_800_000_000, created_at: Time.now - 1.day)
+      kepler_11_system = PlanetarySystem.create!(name: "Kepler-11", light_years_from_earth: 2108, star_age: 3_200_000_000, created_at: Time.now - 2.days)
+      expect(PlanetarySystem.order_by_created_at.to_a).to eq([the_solar_system, tau_ceti_system, kepler_11_system])
+    end
+  end
+
+  # before(:each) do 
+  #   the_solar_system = PlanetarySystem.create(name: "The Solar System", light_years_from_earth: 0, star_age: 4_600_000_000)
+  #   neptune = Planet.create(name: "Neptune", planet_type: "Ice Giant", year_discovered: 1846, confirmed: true, planetary_system_id: the_solar_system.id)
+  #   pluto = Planet.create(name: "Pluto", planet_type: "Dwarf", year_discovered: 1930, confirmed: false, planetary_system_id: the_solar_system.id)
+  #   mercury = Planet.create(name: "Mercury", planet_type: "Terrestrial", year_discovered: 1631, confirmed: true, planetary_system_id: the_solar_system.id)
+  # end
+
+
+  describe '#planets_ordered_alphabetically' do 
+    it 'can order the systems planets alphabetically' do 
+      the_solar_system = PlanetarySystem.create(name: "The Solar System", light_years_from_earth: 0, star_age: 4_600_000_000)
+      neptune = Planet.create(name: "Neptune", planet_type: "Ice Giant", year_discovered: 1846, confirmed: true, planetary_system_id: the_solar_system.id)
+      pluto = Planet.create(name: "Pluto", planet_type: "Dwarf", year_discovered: 1930, confirmed: false, planetary_system_id: the_solar_system.id)
+      mercury = Planet.create(name: "Mercury", planet_type: "Terrestrial", year_discovered: 1631, confirmed: true, planetary_system_id: the_solar_system.id)
+
+      expect(the_solar_system.planets_ordered_alphabetically).to eq([mercury, neptune, pluto])
+    end
+  end
+
+  describe '#ordered_by' do 
+    it 'sorts planets by a give pattern' do 
+      the_solar_system = PlanetarySystem.create(name: "The Solar System", light_years_from_earth: 0, star_age: 4_600_000_000)
+      neptune = Planet.create(name: "Neptune", planet_type: "Ice Giant", year_discovered: 1846, confirmed: true, planetary_system_id: the_solar_system.id)
+      pluto = Planet.create(name: "Pluto", planet_type: "Dwarf", year_discovered: 1930, confirmed: false, planetary_system_id: the_solar_system.id)
+      mercury = Planet.create(name: "Mercury", planet_type: "Terrestrial", year_discovered: 1631, confirmed: true, planetary_system_id: the_solar_system.id)
+
+      expect(the_solar_system.ordered_by(nil)).to eq([neptune, pluto, mercury])
+      expect(the_solar_system.planets.all).to eq([neptune, pluto, mercury])
+      expect(the_solar_system.ordered_by("alphabetical")).to eq([mercury, neptune, pluto])
+      expect(the_solar_system.planets_ordered_alphabetically).to eq([mercury, neptune, pluto])
+      expect(the_solar_system.ordered_by(1900)).to eq([pluto])
+    end
+  end
 end
