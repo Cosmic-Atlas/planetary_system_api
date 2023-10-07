3f49e29cfb23e52715b1e19781104ead8b94b77c
KaraJH <karas.jones@yahoo.com>
2023-10-06 20:14:50 -0600

    feat: Show route for planetary system

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace :api do 
     namespace :v1 do 
-      resources :planetary_systems, only: [:index]
+      resources :planetary_systems, only: [:index, :show]
     end
   end
 end
