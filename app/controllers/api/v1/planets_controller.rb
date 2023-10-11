74e35ec875a2acaf8b925abc80574923b597103d
KaraJH <karas.jones@yahoo.com>
2023-10-11 13:26:18 -0600

    feat: Show action for planet with rendered json

--- app/controllers/api/v1/planets_controller.rb
+++ app/controllers/api/v1/planets_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::PlanetsController < ApplicationController 
   def index 
-    
+    render json: V1::PlanetSerializer.new(Planet.all)
+  end
+
+  def show 
+    render json: V1::PlanetSerializer.new(Planet.find(params[:id]))  
   end
 end
