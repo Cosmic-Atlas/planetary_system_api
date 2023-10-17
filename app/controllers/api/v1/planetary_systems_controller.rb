c1f703211ead88e65db3e727fcfd2e704604d765
KaraJH <karas.jones@yahoo.com>
2023-10-17 09:59:37 -0600

    feat: Render new system

--- app/controllers/api/v1/planetary_systems_controller.rb
+++ app/controllers/api/v1/planetary_systems_controller.rb
@@ -8,6 +8,11 @@ class Api::V1::PlanetarySystemsController < ApplicationController
   end
 
   def create 
-    
+    render json: V1::PlanetarySystemSerializer.new(PlanetarySystem.create!(planetary_system_params))
   end
+
+  private 
+    def planetary_system_params 
+      params.require(:planetary_system).permit(:name, :light_years_from_earth, :star_age)
+    end
 end
