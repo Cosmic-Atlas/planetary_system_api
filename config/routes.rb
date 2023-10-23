ba60ccfbc85083d427ce68abb826f6d644535123
KaraJH <karas.jones@yahoo.com>
2023-10-23 10:27:05 -0600

    feat: Add collection to filter planets by confirmed

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,15 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
       resources :planetary_systems, only: [:index, :show, :create]
-      resources :planets, only: [:index, :show, :create]
+
+      # resources :planets, only: [:index, :show, :create] do 
+      #   # get '/filter/confirmed_planets', :to => 'planets#confirmed_planets'
+      # end
+      resources :planets, only: [:index, :show, :create] do 
+        collection do 
+          get '/filter/confirmed_planets', :to => 'planets#confirmed_planets' 
+        end
+      end
     end
   end
 end
