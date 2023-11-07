7666cbd6a56ead5d5ba9aa18720e252b41b573b2
KaraJH <karas.jones@yahoo.com>
2023-11-07 09:43:52 -0700

    feat: Active REcord REcord Invalid rescue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :invalid_response
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid_response
 
   def invalid_response(exception)
     render json: ErrorSerializer.new(exception).bad_request, status: 404
