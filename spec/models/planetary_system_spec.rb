b5eb5886f6b46a644e74b15a2cd689e3eabad34f
KaraJH <karas.jones@yahoo.com>
2023-11-13 08:52:33 -0700

    test/fix: Add let variables to test attributes

--- spec/models/planetary_system_spec.rb
+++ spec/models/planetary_system_spec.rb
@@ -1,13 +1,25 @@
 require "rails_helper"
 
 RSpec.describe PlanetarySystem do 
+
+  let!(:the_solar_system) {PlanetarySystem.create!(name: "The Solar System", light_years_from_earth: 0, star_age: 4_600_000_000, created_at: Time.now)}
+  let!(:tau_ceti_system) {PlanetarySystem.create!(name: "Tau Ceti", light_years_from_earth: 12, star_age: 5_800_000_000, created_at: Time.now - 1.day)}
+  let!(:kepler_11_system) {PlanetarySystem.create!(name: "Kepler-11", light_years_from_earth: 2108, star_age: 3_200_000_000, created_at: Time.now - 2.days)}
+
    describe '#attributes/columns' do 
     it 'has a name, light years from earth, star age and metal rich star attributes' do
-      solar_system = PlanetarySystem.create(name: "The Solar System", light_years_from_earth: 0, star_age: 4_600_000_000)
+      # solar_system = PlanetarySystem.create(name: "The Solar System", light_years_from_earth: 0, star_age: 4_600_000_000)
 
-      expect(solar_system.name).to eq("The Solar System")
-      expect(solar_system.light_years_from_earth).to eq(0)
-      expect(solar_system.star_age).to eq(4_600_000_000)
+      # expect(solar_system.name).to eq("The Solar System")
+      # expect(solar_system.light_years_from_earth).to eq(0)
+      # expect(solar_system.star_age).to eq(4_600_000_000)
+
+      # solar_system = PlanetarySystem.create(name: "The Solar System", light_years_from_earth: 0, star_age: 4_600_000_000)
+
+      expect(the_solar_system.name).to eq("The Solar System")
+      expect(the_solar_system.light_years_from_earth).to eq(0)
+      expect(the_solar_system.star_age).to eq(4_600_000_000)
+      require 'pry'; binding.pry
       # expect(solar_system.metal_rich_star).to eq(true)
     end
   end
@@ -18,9 +30,10 @@ RSpec.describe PlanetarySystem do
 
   describe '::order_by_created_at' do 
     it 'orders the rows by when they were created at' do 
-      the_solar_system = PlanetarySystem.create!(name: "The Solar System", light_years_from_earth: 0, star_age: 4_600_000_000, created_at: Time.now)
-      tau_ceti_system = PlanetarySystem.create!(name: "Tau Ceti", light_years_from_earth: 12, star_age: 5_800_000_000, created_at: Time.now - 1.day)
-      kepler_11_system = PlanetarySystem.create!(name: "Kepler-11", light_years_from_earth: 2108, star_age: 3_200_000_000, created_at: Time.now - 2.days)
+      # require 'pry'; binding.pry
+    #   the_solar_system = PlanetarySystem.create!(name: "The Solar System", light_years_from_earth: 0, star_age: 4_600_000_000, created_at: Time.now)
+    #   tau_ceti_system = PlanetarySystem.create!(name: "Tau Ceti", light_years_from_earth: 12, star_age: 5_800_000_000, created_at: Time.now - 1.day)
+    #   kepler_11_system = PlanetarySystem.create!(name: "Kepler-11", light_years_from_earth: 2108, star_age: 3_200_000_000, created_at: Time.now - 2.days)
       expect(PlanetarySystem.order_by_created_at.to_a).to eq([the_solar_system, tau_ceti_system, kepler_11_system])
     end
   end
