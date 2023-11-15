f3aedfa121a22c64162d602f89bfad3b0c525d96
KaraJH <karas.jones@yahoo.com>
2023-11-15 09:43:55 -0700

    test: attributes with new variables

--- spec/models/planet_spec.rb
+++ spec/models/planet_spec.rb
@@ -2,14 +2,14 @@ require "rails_helper"
 
 RSpec.describe Planet do 
 
-  #let!(:solar_system) {PlanetarySystem.create!(name: "The Solar System", light_years_from_earth: 0, star_age: 4_600_000_000, created_at: Time.now)}
+  let!(:solar_system) {PlanetarySystem.create!(name: "The Solar System", light_years_from_earth: 0, star_age: 4_600_000_000, created_at: Time.now)}
 
-  # let!(:neptune) {Planet.create(name: "Neptune", planet_type: "Ice Giant", year_discovered: 1846, confirmed: true, planetary_system_id: solar_system.id)}
-  # let!(:pluto) {Planet.create(name: "Pluto", planet_type: "Dwarf", year_discovered: 1930, confirmed: false, planetary_system_id: solar_system.id)}
-  # let!(:mercury) {Planet.create(name: "Mercury", planet_type: "Terrestrial", year_discovered: 1631, confirmed: true, planetary_system_id: solar_system.id)}
-  # let!(:mars) {Planet.create!(name: "Mars", planet_type: "Terrestrial", year_discovered: 1610, confirmed: true, planetary_system_id: solar_system.id)}
-  # let!(:saturn) {Planet.create(name: "Saturn", planet_type: "Gas Giant", year_discovered: 1610, confirmed: true, planetary_system_id: solar_system.id)}
-  # let!(:jupiter) {Planet.create(name: "Jupiter", planet_type: "Gas Giant", year_discovered: 1610, confirmed: true, planetary_system_id: solar_system.id)}
+  let!(:neptune) {Planet.create(name: "Neptune", planet_type: "Ice Giant", year_discovered: 1846, confirmed: true, planetary_system_id: solar_system.id)}
+  let!(:pluto) {Planet.create(name: "Pluto", planet_type: "Dwarf", year_discovered: 1930, confirmed: false, planetary_system_id: solar_system.id)}
+  let!(:mercury) {Planet.create(name: "Mercury", planet_type: "Terrestrial", year_discovered: 1631, confirmed: true, planetary_system_id: solar_system.id)}
+  let!(:mars) {Planet.create!(name: "Mars", planet_type: "Terrestrial", year_discovered: 1610, confirmed: true, planetary_system_id: solar_system.id)}
+  let!(:saturn) {Planet.create(name: "Saturn", planet_type: "Gas Giant", year_discovered: 1610, confirmed: true, planetary_system_id: solar_system.id)}
+  let!(:jupiter) {Planet.create(name: "Jupiter", planet_type: "Gas Giant", year_discovered: 1610, confirmed: true, planetary_system_id: solar_system.id)}
 
   # before(:each) do 
   #   @solar_system = PlanetarySystem.create!(name: "The Solar System", light_years_from_earth: 0, star_age: 4_600_000_000)
@@ -22,12 +22,12 @@ RSpec.describe Planet do
 
   describe '#attributes' do 
     it 'has a name, type, year discovered and confirmed' do 
-      expect(@mars.name).to eq("Mars")
-      expect(@mars.planet_type).to eq("Terrestrial")
-      expect(@mars.year_discovered).to eq(1610)
-      expect(@mars.confirmed).to eq(true)
+      expect(mars.name).to eq("Mars")
+      expect(mars.planet_type).to eq("Terrestrial")
+      expect(mars.year_discovered).to eq(1610)
+      expect(mars.confirmed).to eq(true)
 
-      expect(@mars.planetary_system_id).to eq(@solar_system.id)
+      expect(mars.planetary_system_id).to eq(solar_system.id)
     end
   end
 
