7215372a853ed840a85c8c2807ac8a9c0c2e27e2
KaraJH <karas.jones@yahoo.com>
2023-11-17 09:52:50 -0700

    fix: Remove old test data in planetary system model spec

--- spec/models/planetary_system_spec.rb
+++ spec/models/planetary_system_spec.rb
@@ -12,13 +12,6 @@ RSpec.describe PlanetarySystem do
 
    describe '#attributes/columns' do 
     it 'has a name, light years from earth, star age and metal rich star attributes' do
-      # solar_system = PlanetarySystem.create(name: "The Solar System", light_years_from_earth: 0, star_age: 4_600_000_000)
-
-      # expect(solar_system.name).to eq("The Solar System")
-      # expect(solar_system.light_years_from_earth).to eq(0)
-      # expect(solar_system.star_age).to eq(4_600_000_000)
-
-      # solar_system = PlanetarySystem.create(name: "The Solar System", light_years_from_earth: 0, star_age: 4_600_000_000)
 
       expect(the_solar_system.name).to eq("The Solar System")
       expect(the_solar_system.light_years_from_earth).to eq(0)
@@ -34,40 +27,18 @@ RSpec.describe PlanetarySystem do
 
   describe '::order_by_created_at' do 
     it 'orders the rows by when they were created at' do 
-      # require 'pry'; binding.pry
-    #   the_solar_system = PlanetarySystem.create!(name: "The Solar System", light_years_from_earth: 0, star_age: 4_600_000_000, created_at: Time.now)
-    #   tau_ceti_system = PlanetarySystem.create!(name: "Tau Ceti", light_years_from_earth: 12, star_age: 5_800_000_000, created_at: Time.now - 1.day)
-    #   kepler_11_system = PlanetarySystem.create!(name: "Kepler-11", light_years_from_earth: 2108, star_age: 3_200_000_000, created_at: Time.now - 2.days)
       expect(PlanetarySystem.order_by_created_at.to_a).to eq([the_solar_system, tau_ceti_system, kepler_11_system])
     end
   end
 
-  # before(:each) do 
-  #   the_solar_system = PlanetarySystem.create(name: "The Solar System", light_years_from_earth: 0, star_age: 4_600_000_000)
-  #   neptune = Planet.create(name: "Neptune", planet_type: "Ice Giant", year_discovered: 1846, confirmed: true, planetary_system_id: the_solar_system.id)
-  #   pluto = Planet.create(name: "Pluto", planet_type: "Dwarf", year_discovered: 1930, confirmed: false, planetary_system_id: the_solar_system.id)
-  #   mercury = Planet.create(name: "Mercury", planet_type: "Terrestrial", year_discovered: 1631, confirmed: true, planetary_system_id: the_solar_system.id)
-  # end
-
-
   describe '#planets_ordered_alphabetically' do 
     it 'can order the systems planets alphabetically' do 
-      # the_solar_system = PlanetarySystem.create(name: "The Solar System", light_years_from_earth: 0, star_age: 4_600_000_000)
-      # neptune = Planet.create(name: "Neptune", planet_type: "Ice Giant", year_discovered: 1846, confirmed: true, planetary_system_id: the_solar_system.id)
-      # pluto = Planet.create(name: "Pluto", planet_type: "Dwarf", year_discovered: 1930, confirmed: false, planetary_system_id: the_solar_system.id)
-      # mercury = Planet.create(name: "Mercury", planet_type: "Terrestrial", year_discovered: 1631, confirmed: true, planetary_system_id: the_solar_system.id)
-
       expect(the_solar_system.planets_ordered_alphabetically).to eq([mercury, neptune, pluto])
     end
   end
 
   describe '#ordered_by' do 
     it 'sorts planets by a give pattern' do 
-      # the_solar_system = PlanetarySystem.create(name: "The Solar System", light_years_from_earth: 0, star_age: 4_600_000_000)
-      # neptune = Planet.create(name: "Neptune", planet_type: "Ice Giant", year_discovered: 1846, confirmed: true, planetary_system_id: the_solar_system.id)
-      # pluto = Planet.create(name: "Pluto", planet_type: "Dwarf", year_discovered: 1930, confirmed: false, planetary_system_id: the_solar_system.id)
-      # mercury = Planet.create(name: "Mercury", planet_type: "Terrestrial", year_discovered: 1631, confirmed: true, planetary_system_id: the_solar_system.id)
-
       expect(the_solar_system.ordered_by(nil)).to eq([neptune, pluto, mercury])
       expect(the_solar_system.planets.all).to eq([neptune, pluto, mercury])
       expect(the_solar_system.ordered_by("alphabetical")).to eq([mercury, neptune, pluto])
@@ -78,23 +49,16 @@ RSpec.describe PlanetarySystem do
 
   describe '#search_records' do 
     it 'searches the records by a given keyword and returns the records that match' do 
-      # the_solar_system = PlanetarySystem.create!(name: "Solar System", light_years_from_earth: 0, star_age: 4_600_000_000)
-      # kepler_11_system = PlanetarySystem.create!(name: "Kepler-11", light_years_from_earth: 2108, star_age: 3_200_000_000)
-
       expect(PlanetarySystem.search_records("The Solar System")).to match_array(the_solar_system)
       expect(PlanetarySystem.search_records("Solar")).to match_array(PlanetarySystem.all)
       expect(PlanetarySystem.search_records(nil)).to match_array(PlanetarySystem.all)
     end
 
     it "returns all the planets if no records is found" do 
-      # the_solar_system = PlanetarySystem.create!(name: "Solar System", light_years_from_earth: 0, star_age: 4_600_000_000)
-      # kepler_11_system = PlanetarySystem.create!(name: "Kepler-11", light_years_from_earth: 2108, star_age: 3_200_000_000)
-
       expect(PlanetarySystem.search_records("Cats")).to match_array(PlanetarySystem.all)
     end
 
     it "searches for the records if the planet name was entered lowercase" do 
-      # the_solar_system = PlanetarySystem.create!(name: "Solar System", light_years_from_earth: 0, star_age: 4_600_000_000)
       expect(PlanetarySystem.search_records("the Solar System")).to match_array(the_solar_system)
     end
   end
