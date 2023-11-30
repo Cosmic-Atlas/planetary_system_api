c5ba67ab52c7c32a16e3b06f2cdfd289c75548bc
KaraJH <karas.jones@yahoo.com>
2023-11-30 09:01:44 -0700

    feat: Draft of moon factory

---
+++ spec/factories/moons.rb
@@ -0,0 +1,9 @@
+# FactoryBot.define do 
+#   factory :moon do 
+#     name { Faker::Space.moon}
+#     radius_km { Faker::Number.number(digits: 4)}
+#     rotational_period { Faker::Number.within(range: 1..3)}
+#     magnitude { Faker::Number.within(range: -20.0, to: 20.0)}
+#     planet
+#   end
+# end
