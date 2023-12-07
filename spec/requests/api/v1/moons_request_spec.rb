4e0416697651871db09977bb59a6529d2654fe26
KaraJH <karas.jones@yahoo.com>
2023-12-07 10:04:56 -0700

    test/feat: Add before each data for moon request spec

--- spec/requests/api/v1/moons_request_spec.rb
+++ spec/requests/api/v1/moons_request_spec.rb
@@ -2,6 +2,17 @@ require "rails_helper"
 
 describe "Moons Requests" do 
   describe "Requests" do 
-    
+    before(:each) do 
+      @planetary_system = create(:planetary_system)
+
+      @planet_1 = create(:planet, planetary_system_id: @planetary_system.id, confirmed: true, planet_type: "Gas Giant")
+      @planet_2 = create(:planet, planetary_system_id: @planetary_system.id, confirmed: true, planet_type: "Gas Giant")
+      @planet_3 = create(:planet, planetary_system_id: @planetary_system.id, confirmed: false, planet_type: "Terrestrial")
+
+      @moon_1 = create(:moon, planet_id: @planet_1.id)
+      @moon_2 = create(:moon, planet_id: @planet_1.id)
+      @moon_3 = create(:moon, planet_id: @planet_2.id)
+      @moon_4 - create(:moon, planet_id: @planet_3.id)
+    end
   end
 end
