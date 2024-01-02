5a08eb9754966b21cb5e61c4fc34efd8e1e7321b
KaraJH <karas.jones@yahoo.com>
2024-01-02 07:40:06 -0700

    test/note: Comment out moons by system original method

--- app/models/moon.rb
+++ app/models/moon.rb
@@ -14,7 +14,7 @@ class Moon < ApplicationRecord
   #   Moon.joins(:planet).where(planet: {name: planet_name})
   # end
 
-  def self.moons_by_system(system_name) #scope?
-    Moon.joins(planet: :planetary_system).where(planetary_system: {name: system_name})
-  end
+  # def self.moons_by_system(system_name) #scope?
+  #   Moon.joins(planet: :planetary_system).where(planetary_system: {name: system_name})
+  # end
 end
