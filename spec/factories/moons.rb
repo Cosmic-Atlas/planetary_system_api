a6b3ae443e6c3b0b2e443e2f7ad5cb5c48eaf298
KaraJH <karas.jones@yahoo.com>
2024-01-11 09:28:56 -0700

    feat/fix: Change roational period to float in moon factory

--- spec/factories/moons.rb
+++ spec/factories/moons.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :moon do 
     name { Faker::Space.moon}
     radius_km { Faker::Number.number(digits: 4)}
-    rotational_period { Faker::Number.within(range: 1..3)}
+    rotational_period { Faker::Number.within(range: 1.0..3.0)}
     magnitude { Faker::Number.between(from: -20.0, to: 20.0)}
     planet
   end
