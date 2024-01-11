f45f27dc3441b4276fe2e29f949653ce400056d7
KaraJH <karas.jones@yahoo.com>
2024-01-11 09:28:11 -0700

    test/fix: Change rotational period values to floats

--- spec/models/moon_spec.rb
+++ spec/models/moon_spec.rb
@@ -4,23 +4,21 @@ RSpec.describe Moon do
   let!(:the_solar_system) {PlanetarySystem.create!(name: "The Solar System", light_years_from_earth: 0, star_age: 4_600_000_000, created_at: Time.now)}
 
   let!(:earth) {Planet.create(name: "Earth", planet_type: "Terrestrial", year_discovered: 0, confirmed: true, planetary_system_id: the_solar_system.id)}
-  let!(:luna) {Moon.create!(name: "Luna", radius_km: 1737, rotational_period: 27, magnitude: -12.74, planet_id: earth.id)}
+  let!(:luna) {Moon.create!(name: "Luna", radius_km: 1737, rotational_period: 27.0, magnitude: -12.74, planet_id: earth.id)}
 
   let!(:jupiter) {Planet.create!(name: "Jupiter", planet_type: "Gas Giant", year_discovered: 1610, confirmed: true, planetary_system_id: the_solar_system.id)}
-  let!(:europa) {Moon.create!(name: "Europa", radius_km: 1561, rotational_period: 3, magnitude: 5.29, planet_id: jupiter.id)} #rotational period 3.5
-  let!(:io) {Moon.create!(name: "Io", radius_km: 1821.3, rotational_period: 1, magnitude: 5.02, planet_id: jupiter.id)} #rotational period 1.7
-  let!(:ganymede) {Moon.create!(name: "Ganymede", radius_km: 2631, rotational_period: 7, magnitude: 4.61, planet_id: jupiter.id)}
+  let!(:europa) {Moon.create!(name: "Europa", radius_km: 1561, rotational_period: 3.0, magnitude: 5.29, planet_id: jupiter.id)} #rotational period 3.5
+  let!(:io) {Moon.create!(name: "Io", radius_km: 1821.3, rotational_period: 1.0, magnitude: 5.02, planet_id: jupiter.id)} #rotational period 1.7
+  let!(:ganymede) {Moon.create!(name: "Ganymede", radius_km: 2631, rotational_period: 7.0, magnitude: 4.61, planet_id: jupiter.id)}
 
   let!(:mercury) {Planet.create!(name: "Mercury", planet_type: "Terrestrial", year_discovered: 1631, confirmed: true, planetary_system_id: the_solar_system.id)}
   let!(:venus) {Planet.create!(name: "Venus", planet_type: "Terrestrial", year_discovered: 1610, confirmed: true, planetary_system_id: the_solar_system.id)}
-  #change rotational period to float in db
-
 
   describe "attribues" do 
     it "has attributes" do 
       expect(luna.name).to eq("Luna")
       expect(luna.radius_km).to eq(1737)
-      expect(luna.rotational_period).to eq(27)
+      expect(luna.rotational_period).to eq(27.0)
       expect(luna.magnitude).to eq(-12.74)
     end
   end
@@ -31,7 +29,7 @@ RSpec.describe Moon do
 
   describe "validations" do 
     it "should capitalize the moon name if it was created lowecase" do 
-      lowercase_moon = Moon.create!(name: "moon", radius_km: 1700, rotational_period: 10, magnitude: 10.00, planet_id: earth.id)
+      lowercase_moon = Moon.create!(name: "moon", radius_km: 1700, rotational_period: 10.0, magnitude: 10.00, planet_id: earth.id)
 
       expect(lowercase_moon.name).to eq("Moon")
     end
