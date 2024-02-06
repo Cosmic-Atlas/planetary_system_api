d6e8753cf678dee3cbd3a74999836664edb662d4
KaraJH <karas.jones@yahoo.com>
2024-02-06 07:39:57 -0700

    feat: Search planetary systems action in coontroller

--- app/controllers/api/v1/planetary_systems_controller.rb
+++ app/controllers/api/v1/planetary_systems_controller.rb
@@ -11,6 +11,10 @@ class Api::V1::PlanetarySystemsController < ApplicationController
     render json: V1::PlanetarySystemSerializer.new(PlanetarySystem.create!(planetary_system_params)), status: 201
   end
 
+  def search_planetary_systems 
+    
+  end
+
   private 
     def planetary_system_params 
       params.require(:planetary_system).permit(:name, :light_years_from_earth, :star_age)
