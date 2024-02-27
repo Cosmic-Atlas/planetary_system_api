d1433b32b8c2c32cd81f367bb6023b9a7f074129
KaraJH <karas.jones@yahoo.com>
2024-02-27 10:18:25 -0700

    test/fix: Syntax

--- spec/requests/api/v1/planets_request_spec.rb
+++ spec/requests/api/v1/planets_request_spec.rb
@@ -147,7 +147,7 @@ describe "Planet Requests" do
     end
 
     it "searches for a planet by name" do 
-      get "/api/v1/planets/search_planets?name=#{@planetary_system_1.name}"
+      get "/api/v1/planets/search_planets?name=#{@planetary_1.name}"
 
       expect(response).to be_successful
       expect(response.status).to eq(200)
