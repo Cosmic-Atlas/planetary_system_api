4b9bb231d7b027853299b6bfeec61293e044c2ff
KaraJH <karas.jones@yahoo.com>
2024-02-28 11:47:47 -0700

    test: Parse results to test

--- spec/requests/api/v1/planets_request_spec.rb
+++ spec/requests/api/v1/planets_request_spec.rb
@@ -151,6 +151,12 @@ describe "Planet Requests" do
 
       expect(response).to be_successful
       # expect(response.status).to eq(200)
+
+      search_results = JSON.parse(response.body, symbolize_names: true)
+
+      # expect(search_results).to have_key(:data)
+      # expect(search_results[:data]).to be_an(Array)
+      # expect(search_results[:data].count).to eq(1)
     end
   end
 
