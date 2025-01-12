12325f51a494067ef32b5125d3ebd1706c974df0
KaraJH <karas.jones@yahoo.com>
2025-01-12 14:50:00 -0700

    feat: System planet count in controller

--- app/controllers/api/v1/planetary_systems_controller.rb
+++ app/controllers/api/v1/planetary_systems_controller.rb
@@ -15,6 +15,13 @@ class Api::V1::PlanetarySystemsController < ApplicationController
     render json: V1::PlanetarySystemSerializer.new(PlanetarySystem.search_records(params[:name])), status: 200
   end
 
+  def system_planet_count 
+    # require 'pry'; binding.pry
+    # render json: V1::PlanetarySystemSerializer.new()
+    #make method for planet count in model and test in model spec
+
+  end
+
   private 
     def planetary_system_params 
       params.require(:planetary_system).permit(:name, :light_years_from_earth, :star_age)
